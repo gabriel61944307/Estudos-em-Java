cd1a88c620d4f86b0f07d57a6382e9bd88d3a46c
Gabriel Rodrigues Malaquias <48767872+gabriel61944307@users.noreply.github.com>
2021-09-24 07:54:49 -0300

    Implementando o método abstrato deposita
    O método abstrato definido na classe mãe deve ser implementado em todas classes filhas, logo foi necessário implementar o método deposita na classe ContaPoupanca também

--- bytebank-herdado-conta/ContaPoupanca.java
+++ bytebank-herdado-conta/ContaPoupanca.java
@@ -4,4 +4,9 @@ public class ContaPoupanca extends Conta{
 		// passando agencia e numero para o construtor da classe mãe pois apenas o construtor padrão é herdado automaticamente
 		super(agencia, numero);
 	}
+
+	@Override
+	public void deposita(double valor) {
+		super.saldo += valor;
+	}
 }
