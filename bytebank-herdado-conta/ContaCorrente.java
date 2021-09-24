8138f685868b6348838b3fde561b6a86282e2755
Gabriel Rodrigues Malaquias <48767872+gabriel61944307@users.noreply.github.com>
2021-09-24 07:52:34 -0300

    Classe ContaCorrente implementando Tributavel
    ContaCorrente assinando o contrato Tributavel e implementando os seus métodos, além de implementar o metodo abstrato deposita da sua classe mãe

--- bytebank-herdado-conta/ContaCorrente.java
+++ bytebank-herdado-conta/ContaCorrente.java
@@ -1,5 +1,5 @@
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 	public ContaCorrente(int agencia, int numero) {
 		// passando agencia e numero para o construtor da classe mãe pois apenas o construtor padrão é herdado automaticamente
 		super(agencia, numero);
@@ -9,5 +9,15 @@ public class ContaCorrente extends Conta {
 	public boolean saca(double valor) {
 		double ValorASacar = valor + 0.2;
 		return super.saca(ValorASacar);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		super.saldo += valor;
+	}
+
+	@Override
+	public double getValorImposto() {
+		return super.saldo*0.01;
 	}
 }
